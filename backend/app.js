80e6716959765911a4a2737c2ed6d8b801c41a16
Fera1999 <fernando.ancajima99@gmail.com>
2020-08-20 17:39:04 -0500

    v1.1

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,16 @@ app.use(bodyParser.json());//convertitr a json lo q llegue al body
 
 
 //Rutas
+app.get('/',(req, res)=>{       //req.recibe los datos q envia el cliente//res es la resuesta
+    res.status(200).send(     //send para enviar los datos del res
+        "<h1>hola mundo<h1>"
+    );
+});
 
+app.get('/test',(req, res)=>{       //req.recibe los datos q envia el cliente//res es la resuesta
+    res.status(200).send({      //send para enviar los datos del res
+        message: "hola mundo desde mi api de node.js"
+    });
+});
 //exportar
 module.exports =app;
--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ mongoose.connect('mongodb://localhost:27017/portafolio',{ useNewUrlParser:true, 
             console.log("Se ha establecido la conexion con la bd");
             
             //Creando servidor
-            
+            app.listen(port,()=>{
+                console.log("Corriendo servidor correctamente en: localhost:3700");
+            });
         })
         .catch(err=>console.log(err));

bfb37b9269e0ebf495212457ef4a5621a7911540
Fera1999 <fernando.ancajima99@gmail.com>
2020-09-03 12:59:15 -0500

    Integracion de jquery en angular

--- proyecto-angular/src/app/components/contact/contact.component.ts
+++ proyecto-angular/src/app/components/contact/contact.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// Declaramos las variables para jQuery
+declare var jQuery:any;
+declare var $:any;
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -10,6 +14,11 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $("#logo").click(function(e){
+      e.preventDefault();
+      $("header").css("background","yellow")
+                 .css("height","50px");
+    });
   }
 
 }
